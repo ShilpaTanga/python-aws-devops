4fb647aead1ac9012384dad71063bbe44f9f89e2
ShilpaTanga <96290413+ShilpaTanga@users.noreply.github.com>
2024-08-28 15:39:59 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+set -e
 
+# Pull the Docker image from Docker Hub
+docker pull shilpat18/simple-python-flask-app:latest
+
+# Run the Docker image as a container
+docker run -d -p 5000:50000 shilpat18/simple-python-flask-app:latest
